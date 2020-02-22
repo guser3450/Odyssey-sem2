c31db3a8a97710a1a2a7f4336ddbd23b72d4f3ad
user3450 <gabrielfrigault98@gmail.com>
2020-02-21 22:56:19 -0500

    last version controllers python

---
+++ Rocket_Elevators_Controllers.py
@@ -0,0 +1,72 @@
+def main():
+  column = Column(2,10)
+  elevator = Elevator(elevator1,2,direction)
+  controller.move(elevator.direction,elevator.elevatorNumber[1])
+
+
+#self is the object
+class Column():
+  def __init__(self, elevator, floor):
+    self.elevator = elevator
+    self.floor = floor
+
+    for i in nb_elevator:
+      elevatorList.append(new Elevator)
+
+
+
+  def calculate_gap(self,user_floor):
+
+     if x >= y:
+        result = x - y
+    else:
+        result = y - x
+    return result
+  
+  def move(self, direction, elevator):
+    print("elevator: ", elevator)
+    print("Moving :", direction)
+
+
+
+
+
+class CallButton():
+  def __init__(direction, floor, nb_elevator, nb_floor):
+    self.direction = direction
+    self.floor = floor
+
+
+class Control_panel():
+  def __init__(floor):
+    self.floor = floor
+    self.window
+
+
+class Elevator():
+  def init(self, name, elevator_floor, direction):
+    self.name = name
+    self.activeList = []
+    self.elevator_floor = elevator_floor
+    self.direction = ""
+    self.score = []  
+
+    # for i in range(floorTotal):
+    #     self.floorTotal.append(i)
+    # print(self.floorTotal)  
+
+    # for i in range(elevatorNumber):
+    #     self.elevatorNumber.append(i)
+    # print(self.elevatorNumber)
+
+
+main()
+
+
+
+
+
+
+
+
+
